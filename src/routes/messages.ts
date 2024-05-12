b4d38be063b162c6022286a6708a171dd8ef5739
penguinify <penguinifyminecraft@gmail.com>
2024-05-12 15:04:07 -0700

    control these nuts inside yo mouth

--- src/routes/messages.ts
+++ src/routes/messages.ts
@@ -3,6 +3,7 @@ import { client } from './send';
 
 const messagesHandler = (_: express.Request, res: express.Response) => {
     res.header("Content-Type",'application/json');
+    res.header("Access-Control-Allow-Origin", "*");
 
     const collection = client.db('chat').collection('messages');
     collection.find().toArray().then(messages => {
