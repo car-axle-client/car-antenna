5d97e7dbe04f60c72f9c9b90a1ce42708e753264
penguinify <penguinifyminecraft@gmail.com>
2024-05-12 10:25:25 -0700

    fix message response

--- src/routes/messages.ts
+++ src/routes/messages.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import { chat } from "./send";
 
 const messagesHandler = (_: express.Request, res: express.Response) => {
-    res.send(chat)
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(chat))
 }
 
 export { messagesHandler };
